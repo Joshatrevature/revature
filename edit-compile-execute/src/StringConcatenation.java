cddfc2dcd9c5b5028b6d7467a41269169a0d2bc0
Joshatrevature <joshua.larsen@revature.net>
2021-07-15 13:30:00 -0700

    First set of labs
    yay

---
+++ edit-compile-execute/src/StringConcatenation.java
@@ -0,0 +1,20 @@
+
+public class StringConcatenation {
+	 public static void main(String[] args) {
+	        String beginning = "Hello";
+
+	        String end = " World";
+	        String combo = beginning + end;
+
+	        System.out.println(combo);
+	        
+	        long l = 50L;
+	        System.out.println("The value of l: " + l);
+	        
+	        boolean b = false;
+	        System.out.println(b + " isn't true");
+	        
+	        int x = 1, y =3;
+	        System.out.println(x + y + " is the sum of x and y");
+	    }
+}
